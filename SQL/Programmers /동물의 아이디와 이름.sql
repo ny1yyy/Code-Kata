8edc37903e0d14191f42900a9ad4d5fc2ff5ebec
ny1yyy <135477812+ny1yyy@users.noreply.github.com>
2024-04-01 20:32:04 +0900

    Create 동물의 아이디와 이름.sql

---
+++ SQL/Programmers /동물의 아이디와 이름.sql
@@ -0,0 +1,8 @@
+/*
+동물의 아이디와 이름 : https://school.programmers.co.kr/learn/courses/30/lessons/59403
+동물 보호소에 들어온 모든 동물의 아이디와 이름을 ANIMAL_ID순으로 조회하는 SQL문을 작성.
+*/
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
