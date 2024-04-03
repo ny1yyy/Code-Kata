2ee02efbf22eacda610758327b1d842903d64975
ny1yyy <135477812+ny1yyy@users.noreply.github.com>
2024-04-03 20:37:20 +0900

    Create 삼총사.py

---
+++ Python3 /Programmers /삼총사.py
@@ -0,0 +1,12 @@
+# 삼총사 : https://school.programmers.co.kr/learn/courses/30/lessons/131705
+
+def solution(number):
+    answer = 0
+    
+    for i in range(len(number)-2):
+        for j in range(i+1, len(number)-1):
+            for k in range(j+1, len(number)):
+                if number[i] + number[j] + number[k] == 0:
+                    answer += 1
+
+    return answer
