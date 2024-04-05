c53eb44dc071ee9ae7a94df914a665e4ea41ebe8
ny1yyy <135477812+ny1yyy@users.noreply.github.com>
2024-04-05 15:59:22 +0900

    Create 10773(제로).py

---
+++ Python3 /Baekjoon /10773(제로).py
@@ -0,0 +1,14 @@
+# 10773(제로) : https://www.acmicpc.net/problem/10773
+
+iter_num = int(input())
+num_list = []
+
+for i in range(iter_num):
+  n = int(input())
+  if n > 0:
+    num_list += [n]
+  else:
+    del num_list[-1]   # del 명령문 사용
+  # num_list.pop()     # pop 메서드 사용
+
+print(sum(num_list))
