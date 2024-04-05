9440206e1845b0da926719ba00414bc0d6d73f7e
ny1yyy <135477812+ny1yyy@users.noreply.github.com>
2024-04-05 19:32:51 +0900

    Update TheReport.sql

--- SQL/HackerRank /TheReport.sql
+++ SQL/HackerRank /TheReport.sql
@@ -1,7 +1,6 @@
 # The Report : https://www.hackerrank.com/challenges/the-report/problem?isFullScreen=true
 
 SELECT IF(g.GRADE < 8, 'NULL',  s.NAME), g.GRADE, s.MARKS
-FROM STUDENTS s LEFT JOIN GRADES g ON s.MARKS BETWEEN g.MIN_MARK  AND g.MAX_MARK
-ORDER BY g.GRADE DESC,
-                  CASE WHEN g.GRADE >= 8 THEN s.NAME
+FROM STUDENTS s LEFT JOIN GRADES g ON s.MARKS BETWEEN g.MIN_MARK AND g.MAX_MARK
+ORDER BY g.GRADE DESC, CASE WHEN g.GRADE >= 8 THEN s.NAME
                             WHEN g.GRADE < 8 THEN s.MARKS END ASC;
